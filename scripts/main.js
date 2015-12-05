fe3e0b7f2e760284a13d888d0c98acc24e6f804d
Michał Kopeć <mkopec12@gmail.com>
2015-12-05 15:25:59 +0100

    Finish 3rd function

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,45 @@ function pr1() {
 
     document.getElementById('pr1_result').innerHTML = sum;
 }
+
+function pr2() {
+    var a = 1; // Current fibonacci number
+    var b = 1; // How much to increment a every loop turn
+    var c = 0; // Previous value of a
+    var sum = 0;
+
+    while (a < 4000000) {
+        if (a % 2 === 0)
+            sum = sum + a;
+
+        c = a;
+        a += b;
+        b = c;
+    }
+
+    document.getElementById('pr2_result').innerHTML = sum;
+}
+
+function pr3() {
+    var a = 0;
+    var max = 9999999;
+    var result = 0;
+
+    for (var i = 2; i < max; ++i) {
+        if (isPrime(i) && max % i === 0) {
+            result = i;
+            max /= i;
+            i = 2;
+        }
+    }
+
+    document.getElementById('pr3_result').innerHTML = result;
+}
+
+function isPrime(a) {
+    for (var i = 2; i < a; ++i)
+        if (a % i === 0)
+            return false;
+
+    return true;
+}
